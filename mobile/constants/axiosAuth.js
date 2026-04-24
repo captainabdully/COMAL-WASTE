135dd424a4432b805fcedf43b3dbf05374ada5c0
captainabdully <abdulahmad0305@gmail.com>
2026-04-25 00:27:35 +0300

    change password event

--- mobile/constants/axiosAuth.js
+++ mobile/constants/axiosAuth.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
-//  export const API = axios.create({
-//    baseURL: "http://localhost:5001/api",
-//  });
+// export const API = axios.create({
+//   baseURL: "http://localhost:5001/api",
+// });
 
 export const API = axios.create({
-   baseURL: 'http://54.209.99.13/5001/api',
+  baseURL: "http://54.209.99.13:5001/api",
 });
 
 API.interceptors.request.use(async (config) => {
